27d31cba716d95c494af675f8da535b98bbf5b59
phchestercl <phchester@gmail.com>
2021-09-01 14:42:00 -0400

    seccion 7 antes del build

--- 04-pkm-game/src/components/PokemonOptions.vue
+++ 04-pkm-game/src/components/PokemonOptions.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="optiones-container">
       <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
+          <li 
+            v-for="pokemon in pokemons" :key="pokemon.id"
+            @click="$emit('selection',pokemon.id)"
+          >
+          {{pokemon.name}}   
+          </li>
+
       </ul>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+    props:{
+        pokemons:{type:Array, required:true}
+    }
 }
 </script>
 
--- 04-pkm-game/src/pages/PokemonPage.vue
+++ 04-pkm-game/src/pages/PokemonPage.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
+    <div v-if="!pokemon">
+      <h1>Espere mientras cargamos tu pokemon</h1>
+    </div>
+    <div v-else>
       <h1>Quien es este pokemon</h1>
       <!-- TODO  Poner Imagen -->
-      <PokemonPicture :pokemonID="251" :showPokemon="false"/>
+      <PokemonPicture :pokemonID="pokemon.id" :showPokemon="showPokemon"/>
       <!-- TODOPoner listado de alternativas -->
-      <PokemonOptions />
+      <PokemonOptions 
+        :pokemons="pokemonArr"
+        @selection="checkAnswer"
+      />
+      <template v-if="showAnswer">
+        <h2 > {{message}} </h2>
+        <button @click="newGame">Nuevo Juego</button>
+      </template>
+    </div>
       
   </div>
 </template>
@@ -15,10 +27,50 @@ import PokemonOptions from "@/components/PokemonOptions.vue";
 import getPokemonsOptions from '../helpers/getpokemonoptions';
 
 
-console.log(getPokemonsOptions().then(r=>console.log(r)))
-
 export default {
   components: { PokemonPicture, PokemonOptions },
+  data(){
+    return { 
+      pokemonArr:[],
+      pokemon:null,
+      showPokemon:false,
+      showAnswer:false,
+      message:null
+    }
+  },
+  methods:{
+    async mixPokemonArray(){
+      this.pokemonArr= await getPokemonsOptions()
+      /* console.log(this.pokemonArr) */
+      const rndInt = Math.floor(Math.random()*4)  // Obtiene un valora aleatorio entre 0 y 3
+      console.log(rndInt)
+      this.pokemon=this.pokemonArr[rndInt]
+    },
+    checkAnswer(id){
+      console.log('Pokemon Page llamado '+id)
+      console.log(this.pokemon.id)
+      if (id===this.pokemon.id){
+        this.showPokemon=true
+        this.showAnswer=true
+        this.message=`Correcto es ${this.pokemon.name}`
+      } else {
+        this.showPokemon=false
+        this.showAnswer=true
+        this.message='Te equivocaste !!!'
+      }
+    },
+    newGame(){
+      this.showPokemon=false
+      this.showAnswer=false
+      this.pokemonArr=[]
+      this.pokemon=null
+      this.mixPokemonArray()
+    }
+  },
+  mounted(){
+    
+    this.mixPokemonArray()
+  }
 
 }
 </script>
